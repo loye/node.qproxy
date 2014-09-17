83bc64c010728721e71f85edd62efefe5e8b5b3a
Lijian Qiu <loye.qiu@gmail.com>
2014-09-17 16:09:21 +0800

    add error event listener

--- app.js
+++ app.js
@@ -7,18 +7,22 @@ var net = require('net');
 
 var wss = require('ws.stream');
 
-var url = 'ws://localhost:1337';  //remote server location
-var port = 2000;                  //local listening port
+var url = 'ws://localhost:1337'; //remote server location
+var port = 2000;  //local listening port
 
 //test url
 wss.connect(url).on('connect', function (s) {
   console.log('test url: ' + url + ' -> ' + (s.connected ? 'success' : 'failed'));
   s.close();
+}).on('error', function (err) {
+  console.log(err);
 });
 
 net.createServer(function (socket) {
   wss.connect(url).on('connect', function (s) {
     socket.pipe(s).pipe(socket);
+  }).on('error', function (err) {
+    console.log(err);
   });
 }).listen(port);
 
